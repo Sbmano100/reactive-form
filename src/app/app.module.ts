b9c70dab6809430e02496fc14ae4c182ae85a8ce
Mano Baskaran <mano.baskaran@accolite.com>
2022-12-06 17:45:39 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutModule } from './modules/layout/layout.module';
+import { ActiveVendorComponent } from './modules/vendor/active-vendor/active-vendor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LayoutModule
+    LayoutModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/vendor/active-vendor/active-vendor.component.ts
+++ src/app/modules/vendor/active-vendor/active-vendor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormGroup,FormControl,Validators} from '@angular/forms'; 
 
 @Component({
   selector: 'app-active-vendor',
@@ -9,7 +10,17 @@ export class ActiveVendorComponent implements OnInit {
 
   constructor() { }
 
+  sampleForm = new FormGroup({
+    name: new FormControl('',Validators.required),
+    email: new FormControl('',Validators.required),
+    bio: new FormControl('',Validators.required),
+    domain: new FormControl('',Validators.required)
+  });
   ngOnInit(): void {
   }
 
+  val!:any;
+  onsubmit(){
+    this.val=this.sampleForm.value;
+  }
 }
